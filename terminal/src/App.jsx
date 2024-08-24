db4bef90f4eb126eb7b1100d7cbad3d6d5163aca
Foshati <amirrezafoshati@gmail.com>
2024-08-24 22:46:04 +0330

    create terminal with react and tailwindcss and tauri v1

---
+++ terminal/src/App.jsx
@@ -0,0 +1,105 @@
+import "./App.css";
+import { useEffect, useRef } from "react";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import { invoke } from "@tauri-apps/api";
+import "xterm/css/xterm.css";
+
+function App() {
+  const terminalRef = useRef(null);
+  const terminalInstanceRef = useRef(null);
+  const fitAddonRef = useRef(null);
+
+  useEffect(() => {
+    const initTerminal = async () => {
+      const term = new Terminal({
+        fontFamily: "Fira Code",
+        theme: {
+          background: "black",
+          foreground: "#4A8F72", // Terminal text color
+          cursor: "#4A8F72",
+          selectionBackground: "rgba(74, 143, 114, 0.3)", // Light green selection
+        },
+      });
+
+      const fitAddon = new FitAddon();
+      term.loadAddon(fitAddon);
+
+      term.open(terminalRef.current);
+      terminalInstanceRef.current = term;
+      fitAddonRef.current = fitAddon;
+
+      await initShell();
+      term.onData(writeToPty);
+      window.addEventListener("resize", fitTerminal);
+      fitTerminal();
+
+      readFromPty();
+    };
+
+    initTerminal();
+
+    return () => {
+      window.removeEventListener("resize", fitTerminal);
+      if (terminalInstanceRef.current) {
+        terminalInstanceRef.current.dispose();
+      }
+    };
+  }, []);
+
+  const fitTerminal = async () => {
+    if (fitAddonRef.current && terminalInstanceRef.current) {
+      fitAddonRef.current.fit();
+      await invoke("async_resize_pty", {
+        rows: terminalInstanceRef.current.rows,
+        cols: terminalInstanceRef.current.cols,
+      });
+    }
+  };
+
+  const writeToTerminal = async (data) => {
+    return new Promise((resolve) => {
+      if (terminalInstanceRef.current) {
+        terminalInstanceRef.current.write(data, () => resolve());
+      } else {
+        resolve();
+      }
+    });
+  };
+
+  const writeToPty = (data) => {
+    invoke("async_write_to_pty", { data });
+  };
+
+  const initShell = async () => {
+    try {
+      await invoke("async_create_shell");
+      // Display "bash" upon initialization
+      await writeToPty("bash\n");
+    } catch (error) {
+      console.error("Error creating shell:", error);
+    }
+  };
+
+  const readFromPty = async () => {
+    const data = await invoke("async_read_from_pty");
+
+    if (data) {
+      await writeToTerminal(data);
+    }
+
+    window.requestAnimationFrame(readFromPty);
+  };
+
+  return (
+    <div className="App">
+      <div
+        id="terminal"
+        ref={terminalRef}
+        className="focus:outline-none focus:ring-2 focus:ring-blue-500"
+      ></div>
+    </div>
+  );
+}
+
+export default App;
